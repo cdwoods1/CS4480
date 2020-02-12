a184e1b36e42b47090e065781770af767a239f52
cdwoods1 <39446634+cdwoods1@users.noreply.github.com>
2020-02-12 13:33:34 -0700

    Update HTTPproxy.py

--- HTTPproxy.py
+++ HTTPproxy.py
@@ -5,7 +5,6 @@
 from socket import *
 from optparse import *
 import re
-import sys
 from urlparse import *
 from thread import *
 import hashlib
@@ -13,19 +12,21 @@ import requests
 
 # Constants that determine the amount of concurrent users that can be on the proxy and the socket buffer size.
 MAX_USERS = 100
-BUFFER_SIZE = 2048
+BUFFER_SIZE = 4096
 
 # The set up to get the port number
 parser = OptionParser()
-parser.add_option("-k", )
+parser.add_option("-k", action="store", type="string", dest="key")
+(options, args) = parser.parse_args()
+apiKey = options.key
 serverPort = 2100
+
 
 # Method that begins the proxy to listen for incoming requests.
 def start_proxy():
     server_socket = socket(AF_INET, SOCK_STREAM)
     try:
         # Opening the listening socket.
-        server_socket.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
         server_socket.bind(('', serverPort))
         server_socket.listen(MAX_USERS)
     except Exception:
@@ -35,20 +36,37 @@ def start_proxy():
         try:
             # Code to accept a new client, and start a new thread for the new client.
             connection_socket, client_addr = server_socket.accept()
-            http_request = connection_socket.recv(BUFFER_SIZE)
-            start_new_thread(new_connection, (connection_socket, http_request))
+            start_new_thread(new_connection, (connection_socket,))
         except Exception:
-            http_request.close()
+            connection_socket.close()
 
 
 # Method that is called to handle a new client, whenever they join.
-def new_connection(connection, http_request):
+def new_connection(connection):
     try:
-        
         # First the request is split into each separate header and the request.
+        http_request = connection.recv(BUFFER_SIZE)
+
         request = http_request.split('\n')
         # The rest line is parsed to be checked for validity.
         request_line = request[0].split(' ')
+
+        method = request_line[0]
+        if re.search("^POST", method) or re.search("^HEAD", method):
+            connection.send("HTTP/1.0 501 NOT IMPLEMENTED\r\n"
+                            + "Content-Type: text/html\r\n" +
+                              "Connection: close\r\n\r\n"
+                            + "<html><body>ERROR(501) UNIMPLEMENTED METHOD CALL</body></html>")
+            connection.close()
+            return
+        elif not re.search("^GET", method):
+            connection.send("HTTP/1.0 400 BAD REQUEST\r\n"
+                            + "Content-Type: text/html\r\n" +
+                              "Connection: close\r\n\r\n"
+                            + "<html><body>ERROR(400) MALFORMED REQUEST</body></html>")
+            connection.close()
+            return
+
         header_list = request
         del header_list[0]
 
@@ -56,11 +74,15 @@ def new_connection(connection, http_request):
             header = header.replace("\r", "")
 
             if not re.search("^.*: .*", header) and header is not "":
-                connection.send("HTTP/1.0 400 BAD REQUEST\n"
-                          + "Content-Type: text/html\n\n"
-                          + "<html><body>ERROR(400)\nMALFORMED REQUEST</body></html>\n")
+                connection.send("HTTP/1.0 400 BAD REQUEST\r\n"
+                                + "Content-Type: text/html\r\n" +
+                                  "Connection: close\r\n\r\n"
+                                + "<html><body>ERROR(400) MALFORMED REQUEST</body></html>")
                 connection.close()
                 return
+        if re.search("Accept-Encoding: gzip, deflate", http_request):
+            http_request = http_request.replace("Accept-Encoding: gzip, deflate\r\n", "")
+
         if "Connection: close" not in http_request and "Connection: keep-alive" not in http_request:
             http_request += "Connection: close\r\n"
         elif "Connection: keep-alive" in http_request:
@@ -68,75 +90,64 @@ def new_connection(connection, http_request):
 
         # Test for if the request line only has 3 objects.
         if len(request_line) is not 3:
-            connection.send("HTTP/1.0 400 BAD REQUEST\n"
-                            + "Content-Type: text/html\n\n"
-                            + "<html><body>ERROR(400)\nMALFORMED REQUEST LINE</body></html>\n")
+            connection.send("HTTP/1.0 400 BAD REQUEST\r\n"
+                            + "Content-Type: text/html\r\n" +
+                              "Connection: close\r\n\r\n"
+                            + "<html><body>ERROR(400) MALFORMED REQUEST LINE</body></html>")
             connection.close
             return
         # Code to ensure the request is a GET request.
-        method = request_line[0]
-        if re.search("^POST", method) or re.search("^HEAD", method):
-            connection.send("HTTP/1.0 501 NOT IMPLEMENTED\n"
-                            + "Content-Type: text/html\n\n"
-                            + "<html><body>ERROR(501)\nUNIMPLEMENTED METHOD CALL</body></html>\n")
+
+        url = request_line[1]
+        p = '(?:http.*://)?(?P<host>[^:/ ]+).?(?P<port>[0-9]*).*'
+
+        # Code to ensure that the correct HTTP version is being requested.
+        http_version = request_line[2]
+        if not re.search("HTTP/1.0\r", http_version):
+            connection.send("HTTP/1.0 400 BAD REQUEST\r\n"
+                                + "Content-Type: text/html\r\n" +
+                                  "Connection: close\r\n\r\n"
+                                + "<html><body>ERROR(400) WRONG HTTP VERSION</body></html>")
             connection.close()
             return
-        elif not re.search("^GET", method):
-            connection.send("HTTP/1.0 400 BAD REQUEST\n"
-                            + "Content-Type: text/html\n\n"
-                            + "<html><body>ERROR(400)\nMALFORMED REQUEST</body></html>\n")
-            connection.close()
-            return
-        else:
-            url = request_line[1]
-            # Code to ensure that the correct HTTP version is being requested.
-            http_version = request_line[2]
-            if not re.search("HTTP/1.0\r", http_version):
-                connection.send("HTTP/1.0 400 BAD REQUEST\n"
-                                + "Content-Type: text/html\n\n"
-                                + "<html><body>ERROR(400)\nWRONG HTTP VERSION</body></html>\n")
-                connection.close()
-                return
-            parsed_url = urlparse(url)
+        parsed_url = urlparse(url)
 
-            # Code to ensure the URL is valid.
-            if not parsed_url.scheme:
-                connection.send("HTTP/1.0 400 BAD REQUEST\n"
-                                + "Content-Type: text/html\n\n"
-                                + "<html><body>ERROR(400)\nMALFORMED URL</body></html>\n")
-                connection.close()
-                return
+        # Code to ensure the URL is valid.
 
-            # Code to check whether to use port 80, or a port specified by the user.
+        m = re.search(p, url)
+        hostname = m.group('host')
+        port = m.group('port')
+        if port is '':
             port = 80
-            if parsed_url.port is not None:
-                port = parsed_url.port
+        port = int(port)
 
-            # The URL is appropriately stripped, so it can be used to connect through a socket.
-            url = url.replace("http://", "")
-            sep = '/'
-            hostname = url.split(sep, 1)[0]
-
-            http_socket = socket(AF_INET, SOCK_STREAM)
+        http_socket = socket(AF_INET, SOCK_STREAM)
+        http_socket.settimeout(5)
+        try:
             http_socket.connect((hostname, port))
-            http_socket.send(http_request)
-
-            # The HTTP response code to parse back the response from the socket.
-            while 1:
-                response = http_socket.recv(BUFFER_SIZE)
-                if len(response) > 0:
-
-                    if re.search("HTTP/1.0 200", response) or re.search("HTTP/1.1 200", response):
-                        virus_check_and_send(connection, response)
-                    else:
-                        connection.send(response)
-                else:
-                    break
-
+        except Exception:
             http_socket.close()
             connection.close()
             return
+        http_socket.send(http_request)
+
+        # The HTTP response code to parse back the response from the socket.
+        response = ""
+        while 1:
+            res = http_socket.recv(BUFFER_SIZE)
+            if len(res) > 0:
+                response += res
+            else:
+                if re.search("HTTP/1.0 200", response) or re.search("HTTP/1.1 200", response):
+                    virus_check_and_send(connection, response)
+                else:
+                    connection.send(response)
+                connection.close()
+                http_socket.close()
+                break
+        return
     except Exception, e:
+        print e
         connection.close()
         return
 
@@ -146,15 +157,22 @@ def new_connection(connection, http_request):
 # requested response.
 def virus_check_and_send(conn_socket, response):
     object_test = response.split("\r\n\r\n")
-    params = {'apikey': str(apiKey), 'resource': str(hashlib.md5(object_test[1]).hexdigest())}
+    result = hashlib.md5(object_test[1]).hexdigest()
+    params = {'apikey': str(apiKey), 'resource': result}
     virus_response = requests.get("https://www.virustotal.com/vtapi/v2/file/report", params=params)
-    if re.search("\"detected\": true", virus_response.text):
-        conn_socket.send("content blocked")
+    if re.search("\"response_code\": 0", virus_response.text):
+        conn_socket.send(response)
+        return
+    if re.search("\"positives\": 0", virus_response.text) and re.search("\"response_code\": 1", virus_response.text):
+        conn_socket.send(response)
+    elif re.search("\"detected\": true", virus_response.text):
+        conn_socket.send("HTTP/1.0 200 OK\r\n"
+                         + "Content-Type: text/html\r\n"
+                         + "Connection: close\r\n\r\n"
+                         + "<html><body>content blocked</body></html>")
     else:
         conn_socket.send(response)
     return
 
 
 start_proxy()
-
-
